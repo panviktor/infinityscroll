68f57f310e8ef1edc74a072ac61280ac1b610059
Viktor <35385675+panviktor@users.noreply.github.com>
2023-02-15 21:45:28 +0400

    Initial Commit

---
+++ infinityscroll/infinityscrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  infinityscrollApp.swift
+//  infinityscroll
+//
+//  Created by Viktor on 15.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct infinityscrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
